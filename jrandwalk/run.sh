42890cc844edac21c1155e601c6c75cffec1aae1
Sen Procheta <sprocheta@aneto1.computing.dcu.ie>
2020-08-13 12:33:54 +0100

    script added

--- jrandwalk/run.sh
+++ jrandwalk/run.sh
@@ -1,9 +1,10 @@
 mvn compile
 if [ $# -lt 8]
+then
 	echo "Usage <edge file path> <alpha> <beta> <walk_len> <numWalk> <k> <directed/undirected> <rwalkmode>(Node2vec/Biased_Random_Walk)"	
-
+	exit
+fi
 cd target/classes/
 
 
-$1
 java graphwalk.RWalk $1 $2 $3 $4 $5 $6 $7 $8
